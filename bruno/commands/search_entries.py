84d19604d945fa9a704beb4d5465cf25969691b1
dawsthehassell <hasselldawson@gmail.com>
2025-04-11 16:58:27 -0500

    made search_entries more flexible, added visit_again flag

--- bruno/commands/search_entries.py
+++ bruno/commands/search_entries.py
@@ -7,31 +7,41 @@ DATA_PATH = os.path.join(BASE_DIR, "..", "data", "entry_logs.json")
 
 @click.command(name="search")
 @click.option("--category", default=None, help="Filter by category (ex: restaurant, coffee shop, etc.)")
-@click.option("--term", required=True, help="Search term to look for in entry log")
-def search_entries(term, category):
+@click.option("--term", default=None, help="Search term to look for in entry log")
+@click.option("--visit_again", is_flag=True, help="Search for all results with visit_again = True data")
+def search_entries(term, category, visit_again):
     try:
         with open(DATA_PATH, "r") as f:
             data = json.load(f)
     except FileNotFoundError:
         data = {}
 
-    term = term.strip().lower()
-    if category:
-        category = category.strip().lower()
+    term = term.strip().lower() if term else None
     results = []
 
     if category:
+        category = category.strip().lower()
         if category in data:
-            for entry in data[category]:
-                for value in entry.values():
-                    if term in str(value).lower():
-                        results.append(entry)
+            if term:
+                for entry in data[category]:
+                    for value in entry.values():
+                        if term in str(value).lower():
+                            results.append(entry)
+            else:
+                results.extend(data[category])
+
     else:
         for category_name, entries_list in data.items():
-            for entry in entries_list:
-                for value in entry.values():
-                    if term in str(value).lower():
-                        results.append(entry)
+            if term:
+                for entry in entries_list:
+                    for value in entry.values():
+                        if term in str(value).lower():
+                            results.append(entry)
+            else:
+                results.extend(entries_list)
+            
+    if visit_again:
+        results = [entry for entry in results if entry["visit_again"] == True]
     
     if results:
         for i, entry in enumerate(results, 1):
