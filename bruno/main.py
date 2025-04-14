a51b4e9757d135905bdd7afcfa7dec95872141fe
dawsthehassell <hasselldawson@gmail.com>
2025-04-14 16:51:43 -0500

    remove view_entries, not needed anymore

--- bruno/main.py
+++ bruno/main.py
@@ -1,5 +1,4 @@
 import click
-from bruno.commands.view_entries import view_entries
 from bruno.commands.new_entry import new
 from bruno.commands.search_entries import search_entries
 
@@ -7,7 +6,6 @@ from bruno.commands.search_entries import search_entries
 def cli():
     pass
 
-cli.add_command(view_entries)
 cli.add_command(new)
 cli.add_command(search_entries)
 # haha
