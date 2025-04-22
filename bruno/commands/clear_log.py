2951b86fd36dcac166bc32f2caa85950876f3694
dawsthehassell <hasselldawson@gmail.com>
2025-04-22 14:26:04 -0500

    clear all from log command complete

---
+++ bruno/commands/clear_log.py
@@ -0,0 +1,28 @@
+import click
+import os
+import json
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_DIR, "..", "data", "entry_logs.json")
+
+@click.command(name="clear-all")
+@click.option("--force", is_flag=True, help="Skips all warnings and clears entry log immediately. Use with caution!")
+def clear_all(force):
+    click.echo("Starting Clear All entries from log process...")
+
+    if force:
+        click.echo("Clearing all entries immediately with --force...")
+        with open(DATA_PATH, "w") as f:
+            json.dump({}, f, indent=2)
+        click.echo("All entries have been deleted. Experience Log is now empty.")
+        return
+    
+    clear = click.prompt("Are you sure you want to delete all entries? This cannot be undone! (yes/no)").strip().lower()
+    
+    if clear == "yes":
+        with open(DATA_PATH, "w") as f:
+            json.dump({}, f, indent=2)
+        click.echo("All entries have been deleted. Experience Log is now empty.")
+    else:
+        click.echo("Clear all action cancelled.")
+        return
--- bruno/main.py
+++ bruno/main.py
@@ -1,6 +1,7 @@
 import click
 from bruno.commands.new_entry import new
 from bruno.commands.search_entries import search_entries
+from bruno.commands.clear_log import clear_all
 
 @click.group()
 def cli():
@@ -8,7 +9,7 @@ def cli():
 
 cli.add_command(new)
 cli.add_command(search_entries)
-
+cli.add_command(clear_all)
 
 if __name__ == '__main__':
     cli()
