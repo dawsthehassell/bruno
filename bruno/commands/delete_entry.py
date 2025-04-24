144a28f6d3a6c80642931cfa5b4577736cbab1c8
dawsthehassell <hasselldawson@gmail.com>
2025-04-24 16:58:01 -0500

    delete single entry command complete

--- bruno/commands/delete_entry.py
+++ bruno/commands/delete_entry.py
@@ -19,8 +19,29 @@ def delete_entry():
     category = click.prompt("What experience category is the entry you want to delete from? (bar, restaurant, coffee shop, etc.)")
 
     if category not in data or not data[category]:
-        click.echo("No entries found under category '{category}'")
+        click.echo("No entries found under that category, try another category name!")
         return
     
-    ### keep going on this
-    ### streak only bc today was busy
+    click.echo(f"\nEntries under category '{category}'")
+    for idx, entry in enumerate(data[category]):
+        name = entry.get("name", "Unnamed")
+        date = entry.get("date", "Unknown date")
+        click.echo(f"{idx + 1}. {name} - {date}")
+    try:
+        choice = int(click.prompt("Enter the numer of the entry you'd like to delete"))
+        if not (1 <= choice <= len(data[category])):
+            raise ValueError
+    except ValueError:
+        click.echo("Invalid selection!")
+        return
+    
+    entry = data[category][choice - 1]
+    confirm = click.prompt(f"Are you sure you want to delete '{entry.get('name', 'Unnamed')}'? (yes/no)").strip().lower()
+
+    if confirm == "yes":
+        del data[category][choice - 1]
+        with open(DATA_PATH, "w") as f:
+            json.dump(data, f, indent=2)
+        click.echo("Selected entry has been deleted successfully.")
+    else:
+        click.echo("Deletion cancelled.")
--- bruno/main.py
+++ bruno/main.py
@@ -2,6 +2,7 @@ import click
 from bruno.commands.new_entry import new
 from bruno.commands.search_entries import search_entries
 from bruno.commands.clear_log import clear_all
+from bruno.commands.delete_entry import delete_entry
 
 @click.group()
 def cli():
@@ -10,6 +11,7 @@ def cli():
 cli.add_command(new)
 cli.add_command(search_entries)
 cli.add_command(clear_all)
+cli.add_command(delete_entry)
 
 if __name__ == '__main__':
     cli()
