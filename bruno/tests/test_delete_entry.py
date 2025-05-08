0e5bd0d0c3f290533a33de598acb7bf4ec005cba
dawsthehassell <hasselldawson@gmail.com>
2025-05-08 12:57:39 -0500

    test_delete_entry done

--- bruno/tests/test_delete_entry.py
+++ bruno/tests/test_delete_entry.py
@@ -50,7 +50,83 @@ class TestNewEntry(unittest.TestCase):
         self.assertEqual(len(final_data["restaurant"]), 1)
         self.assertEqual(final_data["restaurant"][0]["name"], "Torchy's")
 
+    def test_delete_entry_cancel(self):
+        self.initial_data = {
+            "restaurant": [
+                {"name": "Chipotle", "date": "4/28/25"},
+                {"name": "Torchy's", "date": "4/27/25"}
+            ]
+        }
+
+        with open(self.data_path, "w") as f:
+            json.dump(self.initial_data, f, indent=2)
+
+        user_input = (
+            "restaurant\n"
+            "1\n"
+            "no\n"
+        )
+
+        result = self.runner.invoke(delete_entry, ["--data-path", self.data_path], input=user_input)
+        self.assertEqual(result.exit_code, 0)
+        self.assertIn("Deletion cancelled.", result.output)
+
+        with open(self.data_path, "r") as f:
+            final_data = json.load(f)
+
+        self.assertEqual(len(final_data["restaurant"]), 2)
+
+    def test_invalid_selection(self):
+        self.initial_data = {
+            "restaurant": [
+                {"name": "Chipotle", "date": "4/28/25"},
+                {"name": "Torchy's", "date": "4/27/25"}
+            ]
+        }
+
+        with open(self.data_path, "w") as f:
+            json.dump(self.initial_data, f, indent=2)
+
+        user_input = (
+            "restaurant\n"
+            "5\n"
+            "no\n"
+        )
+
+        result = self.runner.invoke(delete_entry, ["--data-path", self.data_path], input=user_input)
+        self.assertEqual(result.exit_code, 0)
+        self.assertIn("Invalid selection!", result.output)
+
+        with open(self.data_path, "r") as f:
+            final_data = json.load(f)
+
+        self.assertEqual(len(final_data["restaurant"]), 2)
+
+    def test_invalid_category(self):
+        self.initial_data = {
+            "restaurant": [
+                {"name": "Chipotle", "date": "4/28/25"},
+                {"name": "Torchy's", "date": "4/27/25"}
+            ]
+        }
+
+        with open(self.data_path, "w") as f:
+            json.dump(self.initial_data, f, indent=2)
+
+        user_input = (
+            "bar\n"
+        )
+
+        result = self.runner.invoke(delete_entry, ["--data-path", self.data_path], input=user_input)
+        self.assertEqual(result.exit_code, 0)
+        self.assertIn("No entries found under that category, try again with another category name!", result.output)
+
+        with open(self.data_path, "r") as f:
+            final_data = json.load(f)
+
+        self.assertEqual(len(final_data["restaurant"]), 2)
+        
 if __name__ == "__main__":
     unittest.main()
 
-    # test currently fails btw
+    
